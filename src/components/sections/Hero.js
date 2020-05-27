a3bba516db654f1cec75b1ef1e746ca217a64d03
Paulo Henrique <paulohfcmendes@gmail.com>
2020-05-27 18:32:25 -0300

    fix/ "ecossistema"

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,127 +1,153 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
-import Image from '../elements/Image';
-import Modal from '../elements/Modal';
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
 
 const propTypes = {
-  ...SectionProps.types
-}
+    ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
+    ...SectionProps.defaults,
+};
 
 const Hero = ({
-  className,
-  topOuterDivider,
-  bottomOuterDivider,
-  topDivider,
-  bottomDivider,
-  hasBgColor,
-  invertColor,
-  ...props
+    className,
+    topOuterDivider,
+    bottomOuterDivider,
+    topDivider,
+    bottomDivider,
+    hasBgColor,
+    invertColor,
+    ...props
 }) => {
+    const [videoModalActive, setVideomodalactive] = useState(false);
+    const [EquipeModalActive, setEquipemodalactive] = useState(false);
 
-  const [videoModalActive, setVideomodalactive] = useState(false);
-  const [EquipeModalActive, setEquipemodalactive] = useState(false);
+    const openModal = (e) => {
+        e.preventDefault();
+        setVideomodalactive(true);
+    };
 
-  const openModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(true);
-  }
+    const closeModal = (e) => {
+        e.preventDefault();
+        setVideomodalactive(false);
+    };
 
-  const closeModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(false);
-  } 
-  
-  const openEquipeModal = (e) => {
-    e.preventDefault();
-    setEquipemodalactive(true);
-  }
+    const openEquipeModal = (e) => {
+        e.preventDefault();
+        setEquipemodalactive(true);
+    };
 
-  const closeEquipeModal = (e) => {
-    e.preventDefault();
-    setEquipemodalactive(false);
-  } 
+    const closeEquipeModal = (e) => {
+        e.preventDefault();
+        setEquipemodalactive(false);
+    };
 
-  const outerClasses = classNames(
-    'hero section center-content',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
-    className
-  );
+    const outerClasses = classNames(
+        "hero section center-content",
+        topOuterDivider && "has-top-divider",
+        bottomOuterDivider && "has-bottom-divider",
+        hasBgColor && "has-bg-color",
+        invertColor && "invert-color",
+        className
+    );
 
-  const innerClasses = classNames(
-    'hero-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
-  );
+    const innerClasses = classNames(
+        "hero-inner section-inner",
+        topDivider && "has-top-divider",
+        bottomDivider && "has-bottom-divider"
+    );
 
-  return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
-      <div className="container-sm">
-        <div className={innerClasses}>
-          <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              <span  className="text-color-primary">SOS UnB</span> contra a Covid-19
-            </h1>
-            <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Um ecossitema pensado para proteger da epidemia global causada pela Covid-19 todos aqueles que integram a comunidade da Universidade de Brasília.
-                </p>
-              <div className="reveal-from-bottom" data-reveal-delay="600">
-                <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://googleplay.com">
-                    Baixar o Aplicativo
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    Seja Parceiro
-                    </Button>
-                </ButtonGroup>
-              </div>
+    return (
+        <section {...props} className={outerClasses}>
+            <div className="container-sm">
+                <div className={innerClasses}>
+                    <div className="hero-content">
+                        <h1
+                            className="mt-0 mb-16 reveal-from-bottom"
+                            data-reveal-delay="200"
+                        >
+                            <span className="text-color-primary">SOS UnB</span>{" "}
+                            contra a Covid-19
+                        </h1>
+                        <div className="container-xs">
+                            <p
+                                className="m-0 mb-32 reveal-from-bottom"
+                                data-reveal-delay="400"
+                            >
+                                Um ecossistema pensado para proteger da epidemia
+                                global causada pela Covid-19 todos aqueles que
+                                integram a comunidade da Universidade de
+                                Brasília.
+                            </p>
+                            <div
+                                className="reveal-from-bottom"
+                                data-reveal-delay="600"
+                            >
+                                <ButtonGroup>
+                                    <Button
+                                        tag="a"
+                                        color="primary"
+                                        wideMobile
+                                        href="https://googleplay.com"
+                                    >
+                                        Baixar o Aplicativo
+                                    </Button>
+                                    <Button
+                                        tag="a"
+                                        color="dark"
+                                        wideMobile
+                                        href="https://github.com/cruip/open-react-template/"
+                                    >
+                                        Seja Parceiro
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="hero-figure reveal-from-bottom illustration-element-01"
+                        data-reveal-value="20px"
+                        data-reveal-delay="800"
+                    >
+                        <a
+                            data-video="./../../assets/images/video-placeholder.jpg"
+                            href="#0"
+                            aria-controls="video-modal"
+                            onClick={openModal}
+                        >
+                            <Image
+                                className="has-shadow"
+                                src={require("./../../assets/images/background.png")}
+                                alt="Hero"
+                                width={896}
+                                height={504}
+                            />
+                        </a>
+                    </div>
+                    <Modal
+                        id="video-modal"
+                        show={videoModalActive}
+                        handleClose={closeModal}
+                        video="'./../../assets/images/Materia-Jornal-DF2.mp4"
+                        videoTag="iframe"
+                    />
+                    <Modal
+                        id="equipe-modal"
+                        show={EquipeModalActive}
+                        handleClose={closeEquipeModal}
+                        video="'./../../assets/images/Materia-Jornal-DF2.mp4"
+                        videoTag="iframe"
+                    />
+                </div>
             </div>
-          </div>
-          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-            <a
-              data-video="./../../assets/images/video-placeholder.jpg"
-              href="#0"
-              aria-controls="video-modal"
-              onClick={openModal}
-            >
-              <Image
-                className="has-shadow"
-                src={require('./../../assets/images/background.png')}
-                alt="Hero"
-                width={896}
-                height={504} />
-            </a>
-          </div>
-          <Modal
-            id="video-modal"
-            show={videoModalActive}
-            handleClose={closeModal}
-            video="'./../../assets/images/Materia-Jornal-DF2.mp4"
-            videoTag="iframe" />
-          <Modal
-            id="equipe-modal"
-            show={EquipeModalActive}
-            handleClose={closeEquipeModal}
-            video="'./../../assets/images/Materia-Jornal-DF2.mp4"
-            videoTag="iframe" />
-        </div>
-      </div>
-    </section>
-  );
-}
+        </section>
+    );
+};
 
 Hero.propTypes = propTypes;
 Hero.defaultProps = defaultProps;
