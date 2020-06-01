516bd8fddbc47221c1f2f33b1f7b6f043304e4c8
Paulo Henrique <paulohfcmendes@gmail.com>
2020-06-01 10:51:15 -0300

    fix/ title "SOS"

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -72,7 +72,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              <span  className="text-color-primary">SOS UnB</span> contra a Covid-19
+              <p  className="text-color-primary">SOS UnB</p> contra a Covid-19
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
